b9af7e358d821b7031b6daccaea73d29523326d4
SyedKamranGH <syed.kam.ahmed@gmail.com>
2024-08-20 16:31:51 +0500

    Create basic APIs

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,52 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  /* 
+    List of routes
+    GET /users
+    GET /users/:id
+    POST /users
+    PATCH /users/:id
+    DELETE /users/:id
+    */
+
+  @Get() // get all users
+  findAll() {
+    return [];
+  }
+
+  // Order matters for dynamic routing
+  //   @Get('interns')
+  //   findAllInterns() {
+  //     return [];
+  //   }
+
+  @Get(':id') //get specific user
+  findOne(@Param('id') id: string) {
+    return { id };
+  }
+
+  @Post()
+  create(@Body() user: object) {
+    return user;
+  }
+
+  @Patch(':id') //get specific user
+  update(@Param('id') id: string, @Body() userUpdate: object) {
+    return { id, ...userUpdate };
+  }
+
+  @Delete(':id') //get specific user
+  delete(@Param('id') id: string) {
+    return { id };
+  }
+}
